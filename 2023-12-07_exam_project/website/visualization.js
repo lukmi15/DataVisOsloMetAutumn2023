c38a09faa9ccda20d39a3bfcf36b6d0234bca8cb
lukmi15 <lukas.mirow@web.de>
2023-12-04 13:52:47 +0100

    Finished scaling the visualization

--- 2023-12-07_exam_project/website/visualization.js
+++ 2023-12-07_exam_project/website/visualization.js
@@ -7,11 +7,20 @@ Date of creation	: 2023-12-03
 const PARAMS = ["show_attack", "show_base_egg_steps", "show_base_happiness", "show_capture_rate", "show_defense", "show_experience_growth", "show_height_m", "show_hp", "show_percentage_male", "show_sp_attack", "show_sp_defense", "show_speed", "show_weight_kg"];
 const SPRITE_WIDTH = 100;
 const SPRITE_HEIGHT = 100;
-const MIN_BUBBLE_SIZE = 10;
-const MAX_BUBBLE_SIZE = 25;
+const MIN_BUBBLE_SIZE = 25;
+const MAX_BUBBLE_SIZE = 250;
 const PARAMS_PREFIX = 5;
 const BUTTONS_POSTFIX = 4;
+const VISUALIZATION_CONTAINER_ID = "bubble_charts"
 
+
+//Pad numbers with zeros
+//Source: https://stackoverflow.com/questions/6220693/string-format-in-javascript
+function zeroPad(nr, base)
+{
+	var len = (String(base).length - String(nr).length)+1;
+	return len > 0? new Array(len).join('0')+nr : nr;
+}
 
 //Update visualization
 function update_vis()
@@ -43,50 +52,60 @@ function update_vis()
 			.range([MIN_BUBBLE_SIZE, MAX_BUBBLE_SIZE]);
 	console.debug(bubble_data_arr)
 
-	//Select the SVG container
-	const svg = d3.select('#bubble-chart');
+	//Create visualizations
+	/* for (var j = 0; j < DATA.length; j++) */
+	for (var j = 0; j < 50; j++)
+	{
 
-	//Remove any existing elements
-	svg.selectAll('*').remove();
+		//Create and select an SVG container
+		document.getElementById(VISUALIZATION_CONTAINER_ID).innerHTML += '<svg id="bubble_chart_' + j + '" class="bubble_chart" width="300" height="300"></svg>';
+		const svg = d3.select('#bubble_chart_' + j);
 
-	//Get the center coordinates of the chart
-	const center_x = svg.attr('width') / 2;
-	const center_y = svg.attr('height') / 2;
+		//Remove any existing elements
+		svg.selectAll('*').remove();
 
-	//Cirlcular pattern
-	const angleStep = (2 * Math.PI) / bubble_data_arr.length;
+		//Get the center coordinates of the chart
+		const center_x = svg.attr('width') / 2;
+		const center_y = svg.attr('height') / 2;
 
-	//Draw lines from bubbles to the center
-	svg.selectAll('line')
-		.data(bubble_data_arr)
-		.enter()
-		.append('line')
-		.attr('class', 'line')
-		.attr('x1', center_x)
-		.attr('y1', center_y)
-		.attr('x2', (d, i) => center_x + Math.cos(i * angleStep) * SPRITE_WIDTH)
-		.attr('y2', (d, i) => center_y + Math.sin(i * angleStep) * SPRITE_HEIGHT);
+		//Cirlcular pattern
+		const angleStep = (2 * Math.PI) / bubble_data_arr.length;
 
-	//Create circles for each data point in a circular pattern
-	var bubbles = svg.selectAll('circle').data(bubble_data_arr).enter()
-	bubbles.append('circle')
-		.attr('class', 'bubble')
-		.attr('cx', (d, i) => center_x + Math.cos(i * angleStep) * SPRITE_WIDTH)
-		.attr('cy', (d, i) => center_y + Math.sin(i * angleStep) * SPRITE_HEIGHT)
-		.attr('r', d => d.radius_scale(d.values[i]))
-		.attr('title', d => d.name)
-		.on('mouseover', showTooltip)
-		.on('mouseout', hideTooltip);
+		//Draw lines from bubbles to the center
+		svg.selectAll('line')
+			.data(bubble_data_arr)
+			.enter()
+			.append('line')
+			.attr('class', 'line')
+			.attr('x1', center_x)
+			.attr('y1', center_y)
+			.attr('x2', (d, i) => center_x + Math.cos(i * angleStep) * SPRITE_WIDTH)
+			.attr('y2', (d, i) => center_y + Math.sin(i * angleStep) * SPRITE_HEIGHT);
 
-	//Add an image to the center
-	svg.append('image')
-		.attr('x', center_x - SPRITE_WIDTH/2)
-		.attr('y', center_y - SPRITE_HEIGHT/2)
-		.attr('width', SPRITE_WIDTH)
-		.attr('height', SPRITE_HEIGHT)
-		.attr('xlink:href', 'sprites/poke_capture_0258_000_mf_n_00000000_f_n.png')
-		.style('cursor', 'pointer')
-		.on('click', showCharacteristicsTooltip);
+		//Create circles for each data point in a circular pattern
+		var bubbles = svg.selectAll('circle').data(bubble_data_arr).enter()
+		bubbles.append('circle')
+			.attr('class', 'bubble')
+			.attr('cx', (d, i) => center_x + Math.cos(i * angleStep) * SPRITE_WIDTH)
+			.attr('cy', (d, i) => center_y + Math.sin(i * angleStep) * SPRITE_HEIGHT)
+			.attr('r', d => Math.sqrt(d.radius_scale(d.values[j])))
+			.attr('title', d => document.getElementById("show_" + d.name + "_btn").innerHTML + ": " + d.values[j])
+			.attr('index', j)
+			.on('mouseover', showTooltip)
+			.on('mouseout', hideTooltip);
+
+		//Add an image to the center
+		svg.append('image')
+			.attr('class', 'bubble_img')
+			.attr('x', center_x - SPRITE_WIDTH/2)
+			.attr('y', center_y - SPRITE_HEIGHT/2)
+			.attr('width', SPRITE_WIDTH)
+			.attr('height', SPRITE_HEIGHT)
+			.attr('xlink:href', 'sprites/' + pokeid2spritepath(j))
+			.style('cursor', 'pointer')
+			.on('click', showCharacteristicsTooltip);
+
+	}
 }
 
 //Function to show the tooltip
@@ -94,7 +113,7 @@ function showTooltip(event, d)
 {
 	const tooltip = document.getElementById('tooltip');
 	const tooltipText = document.getElementById('tooltip-text');
-	tooltipText.textContent = d.name;
+	tooltipText.textContent = d.getAttribute("title");
 	tooltip.style.left = (event.pageX + 10) + 'px';
 	tooltip.style.top = (event.pageY - 20) + 'px';
 	tooltip.style.display = 'block';
@@ -130,13 +149,40 @@ function btn_pressed(caller)
 }
 
 
-//Add an event listener to hide the tooltip when clicking outside of the image
-document.addEventListener
+// Add event listeners to the container element
+document.getElementById('bubble_charts').addEventListener
+(
+	'mouseover',
+	function (event)
+	{
+		if (event.target.classList.contains('bubble'))
+			showTooltip(event, event.target);
+	}
+);
+document.getElementById('bubble_charts').addEventListener
+(
+	'mouseout',
+	function (event)
+	{
+		if (event.target.classList.contains('bubble'))
+			hideTooltip();
+	}
+);
+document.getElementById('bubble_charts').addEventListener
 (
 	'click',
 	function (event)
 	{
-		if (!event.target.closest('#bubble-chart'))
+		if (event.target.classList.contains('bubble_img'))
+			showCharacteristicsTooltip(event);
+	}
+);
+document.getElementById('bubble_charts').addEventListener
+(
+	'click',
+	function (event)
+	{
+		if (!event.target.closest('.bubble_img'))
 			hideTooltip();
 	}
 );
